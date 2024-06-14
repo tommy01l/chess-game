48073b5ea4083a98b85fddd2591bb39ede9e8e21
Tommy <tll68@cornell.edu>
2024-06-14 12:30:22 -0500

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/chess-game/',
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
-  build: {
-    assetsDir: 'assets',
-    outDir: 'dist',
-    manifest: true,
+  server: {
+    fs: {
+      strict: false,
+    },
   },
 });
