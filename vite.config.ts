3d203ebe23d8e586d3d1f12de09c757a3ce0fb65
Tommy <tll68@cornell.edu>
2024-06-14 12:43:40 -0500

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/chess-game/',
-  server: {
-    fs: {
-      strict: false,
-    },
+  build: {
+    outDir: 'dist',
   },
 });
